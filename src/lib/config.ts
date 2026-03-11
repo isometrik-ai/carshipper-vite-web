242a52f22ebb6f1a49dc57186f55ec30b7b0fd51
sowmyasv <sowmya@yourstory.com>
2026-03-11 17:29:20 +0530

    Fixed : website
    1) api url is changes
    2) added country , address line 1 , address line 2 and customer phone number and country code for quotation creation
    
    Date: 11-03-2026
    Dev: Sowmya SV

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -6,9 +6,9 @@ export const DEFAULT_COUNTRY_CODE = 'us';
 export const DEFAULT_COUNTRY_CODE_SYMBOL = '+1';
 export const DEFAULT_CURRENCY_SYMBOl = "$";
 export const DEFAULT_CURRENCY_CODE = "USD";
-const rawApiBase = process.env.NEXT_PUBLIC_API_BASE_URL || "http://5.161.218.11:7008";
+const rawApiBase = process.env.NEXT_PUBLIC_API_BASE_URL || "http://5.161.218.11:7009";
 export const API_BASE_URL =
   typeof window !== "undefined" && window.location?.protocol === "https:"
     ? "/api/backend"
     : rawApiBase;
-export const MAIN_API_URL = `${API_BASE_URL}/v1`;
+export const MAIN_API_URL = `${API_BASE_URL}/v2`;
